603c3ba037798661efa1277d0bb5e4cc7ef0fa8d
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-05-31 21:25:49 +0900

    Feat: count tap by using Userdefaults

--- IExpense/IExpense/ContentView.swift
+++ IExpense/IExpense/ContentView.swift
@@ -8,31 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var numbers = [Int]()
-    @State private var number = 0
+    @State private var tapCount = UserDefaults.standard.integer(forKey: "tapCount")
     var body: some View {
-        NavigationStack {
-            VStack {
-                List {
-                    ForEach(numbers, id: \.self) {
-                        Text("number \($0)")
-                    }
-                    .onDelete(perform: { indexSet in
-                        removeElement(at: indexSet)
-                    })
-                }
-                Button("Add number") {
-                    numbers.append(number)
-                    number += 1
-                }
-            }
-            .toolbar {
-                EditButton()
-            }
+        Button("tap count: \(tapCount)") {
+            tapCount += 1
+            UserDefaults.standard.set(tapCount, forKey: "tapCount")
         }
-    }
-    func removeElement(at offset: IndexSet) {
-        numbers.remove(atOffsets: offset)
     }
 }
 
