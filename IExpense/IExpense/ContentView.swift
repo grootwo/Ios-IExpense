e2f4dfbcf905e84858d100311e0e1dfe32d42d62
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-06-01 16:21:42 +0900

    Design: show property of the item

--- IExpense/IExpense/ContentView.swift
+++ IExpense/IExpense/ContentView.swift
@@ -41,7 +41,16 @@ struct ContentView: View {
         NavigationStack {
             List {
                 ForEach(expenses.items) { item in
-                    Text("\(item.name)")
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.name)
+                                .font(.headline)
+                            Text(item.type)
+                                .font(.subheadline)
+                        }
+                        Spacer()
+                        Text(item.amount, format: .currency(code: "USD"))
+                    }
                 }
                 .onDelete(perform: { indexSet in
                     removeAtOffset(at: indexSet)
