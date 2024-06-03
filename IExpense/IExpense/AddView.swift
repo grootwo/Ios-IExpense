f17e60ac94a78d69241e62d4f232782b6e77913b
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-06-03 15:33:32 +0900

    Feat: select currency locale

--- IExpense/IExpense/AddView.swift
+++ IExpense/IExpense/AddView.swift
@@ -11,14 +11,16 @@ struct AddView: View {
     @Environment(\.dismiss) var dismiss
     var expenses: Expenses
     static let currencyFormatter: NumberFormatter = {
-            let formatter = NumberFormatter()
-            formatter.numberStyle = .currency
-            formatter.locale = Locale(identifier: "en_US")
-            return formatter
-        }()
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = Locale(identifier: "en_US")
+        return formatter
+    }()
+    @State private var currencyLocale = "en_US"
+    let currencyLocales = ["USD", "EUR", "KRW"]
     @State private var name = ""
-    @State private var type = "Personal"
     @State private var amount = 0.0
+    @State private var type = "Personal"
     let types = ["Personal", "Business"]
     var body: some View {
         NavigationStack {
@@ -27,6 +29,20 @@ struct AddView: View {
                 Picker("type", selection: $type) {
                     ForEach(types, id: \.self) {
                         Text($0)
+                    }
+                }
+                Picker("currency", selection: $currencyLocale) {
+                    ForEach(currencyLocales, id: \.self) {
+                        Text($0)
+                    }
+                }
+                .onChange(of: currencyLocale) { oldValue, newValue in
+                    if newValue == "USD" {
+                        AddView.currencyFormatter.locale = Locale(identifier: "en_US")
+                    } else if newValue == "EUR" {
+                        AddView.currencyFormatter.locale = Locale(identifier: "fr_FR")
+                    } else {
+                        AddView.currencyFormatter.locale = Locale(identifier: "ko_KR")
                     }
                 }
                 TextField("amount", value: $amount, formatter: AddView.currencyFormatter)
