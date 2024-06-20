a23cd8407128f658bb55a8eea8504e8c3a319561
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-06-20 15:44:37 +0900

    Fix: show proper NavigationLink

--- HabitTracker/HabitTracker/ContentView.swift
+++ HabitTracker/HabitTracker/ContentView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    var habits = [Habit(title: "매일 낙법 연습하기", description: "매일 전방/측방/후방 낙법 최소 10번 연습하기 "), Habit(title: "매일 낙법 연습하기", description: "매일 전방/측방/후방 낙법 최소 10번 연습하기 ")]
+    var habits = [Habit(title: "매일 낙법 연습하기", description: "매일 전방/측방/후방 낙법 최소 10번 연습하기 "), Habit(title: "낙법 연습하기", description: "전방/측방/후방 낙법 최소 10번 연습하기 ")]
     var body: some View {
         NavigationStack {
             List {
