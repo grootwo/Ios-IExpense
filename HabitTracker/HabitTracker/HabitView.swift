edf003476e93ab1821933efa383889824a65239d
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-06-20 16:18:54 +0900

    Feat: setHabitView with colorful button

--- HabitTracker/HabitTracker/HabitView.swift
+++ HabitTracker/HabitTracker/HabitView.swift
@@ -10,7 +10,23 @@ import SwiftUI
 struct HabitView: View {
     @State var habit: Habit
     var body: some View {
-        Text(habit.title)
+        VStack {
+            Spacer()
+            Text(habit.description)
+                .font(.title3)
+            Spacer()
+            Button(action: {
+                habit.successCount += 1
+            }, label: {
+                    Label("오늘도 성공!", systemImage: "checkmark.circle")
+                        .font(.title)
+                        .foregroundColor(Color.random())
+            })
+        }
+        .padding()
+        .toolbar {
+            Text("달성 횟수: \(habit.successCount)")
+        }
     }
 }
 
