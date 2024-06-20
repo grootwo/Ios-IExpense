1a9f0c3488ffdb5c192a85f04d0040c5bb712026
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-06-20 16:34:42 +0900

    Feat: set data as State or binding with error that binding value does not change immediately

--- HabitTracker/HabitTracker/ContentView.swift
+++ HabitTracker/HabitTracker/ContentView.swift
@@ -8,19 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
-    var habits = [Habit(title: "매일 낙법 연습하기", description: "매일 전방/측방/후방 낙법 최소 10번 연습하기 "), Habit(title: "낙법 연습하기", description: "전방/측방/후방 낙법 최소 10번 연습하기 ")]
+    @State var habits = [Habit(title: "매일 낙법 연습하기", description: "매일 전방/측방/후방 낙법 최소 10번 연습하기 "), Habit(title: "낙법 연습하기", description: "전방/측방/후방 낙법 최소 10번 연습하기 ")]
     var body: some View {
         NavigationStack {
             List {
-                ForEach(habits, id: \.self) { habit in
-                    NavigationLink(destination: HabitView(habit: habit), label: {
+                ForEach($habits, id: \.self) { $habit in
+                    NavigationLink(destination: HabitView(habit: $habit), label: {
                         HStack {
-                            Text(habit.title)
+                            Text("\(habit.title)")
                                 .font(.headline)
                             Spacer()
                             Image(systemName: "\(habit.successCount).circle.fill")
                         }
                     })
+                    .tint(.secondary)
                 }
             }
         }
--- HabitTracker/HabitTracker/HabitView.swift
+++ HabitTracker/HabitTracker/HabitView.swift
@@ -8,28 +8,28 @@
 import SwiftUI
 
 struct HabitView: View {
-    @State var habit: Habit
+    @Binding var habit: Habit
+    @State var buttonColor = Color.random()
     var body: some View {
         VStack {
+            Text("달성 횟수: \(habit.successCount)")
             Spacer()
             Text(habit.description)
                 .font(.title3)
             Spacer()
             Button(action: {
                 habit.successCount += 1
+                buttonColor = Color.random()
             }, label: {
                     Label("오늘도 성공!", systemImage: "checkmark.circle")
                         .font(.title)
-                        .foregroundColor(Color.random())
+                        .foregroundColor(buttonColor)
             })
         }
         .padding()
-        .toolbar {
-            Text("달성 횟수: \(habit.successCount)")
-        }
     }
 }
 
 #Preview {
-    HabitView(habit: Habit(title: "유도 업어 치기", description: "매일 상대방 아무나 10번 업어 치기"))
+    HabitView(habit: .constant(Habit(title: "유도 업어 치기", description: "매일 상대방 아무나 10번 업어 치기")))
 }
