010ff51fa23ef1fa924a10645abc51bb9ad5207a
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-06-20 15:28:49 +0900

    Feat: create Habit struct

---
+++ HabitTracker/HabitTracker/Habit.swift
@@ -0,0 +1,14 @@
+//
+//  Habit.swift
+//  HabitTracker
+//
+//  Created by Groo on 6/20/24.
+//
+
+import Foundation
+
+struct Habit {
+    var title: String
+    var description: String
+    var successCount: Int
+}
