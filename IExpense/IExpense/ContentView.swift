518eef2663e416dd3c2caae8773751a4532ebe1d
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-07-05 14:28:44 +0900

    Refactor: delete comment

--- IExpense/IExpense/ContentView.swift
+++ IExpense/IExpense/ContentView.swift
@@ -8,34 +8,6 @@
 import SwiftUI
 import SwiftData
 
-//struct ExpenseItem: Identifiable, Codable {
-//    var id = UUID()
-//    let name: String
-//    let type: String
-//    let amount: Double
-//    let currencyCode: String
-//}
-//
-//@Observable
-//class Expenses {
-//    var items = [ExpenseItem]() {
-//        didSet {
-//            if let encoded = try? JSONEncoder().encode(items) {
-//                UserDefaults.standard.set(encoded, forKey: "Items")
-//            }
-//        }
-//    }
-//    
-//    init() {
-//        if let savedItems = UserDefaults.standard.data(forKey: "Items") {            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
-//            items = decodedItems
-//            return
-//        }
-//        }
-//        items = []
-//    }
-//}
-
 struct ContentView: View {
     @Environment(\.modelContext) var modelContext
     @Query var expenses: [Expense]    
